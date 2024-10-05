bbf3a6c7b0f1fe86513add89cc2caa8ee1277610
Ajinkya Patil <ajinkyapatil240499@gmail.com>
2024-10-05 13:09:51 +0530

    it is simple JSX project

---
+++ Project_01/src/Demo.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Demo() {
+  return (
+    <>
+      <div className="image-class">
+        <img src="/game-of-thrones-pictures.jpg" alt="Game of Thrones" />
+      </div>
+      <div>
+        <h2>Name: Game of Thrones</h2>
+        <h4>Rating : 8.5/10</h4>
+        <h5>
+          Summary :The series focuses on several noble houses of Westeros and a
+          complicated, multiparty war for the Iron Throne that features shifting
+          conflicts, alliances, and betrayals.
+        </h5>
+      </div>
+    </>
+  );
+}
+
+export default Demo;
